58c04d090b03541871ec6d6ed7a9a2ddcdedd430
ekansh goel <ekanshgoel@123.com>
2019-03-08 03:03:27 +0530

    data added

--- app/src/main/java/com/example/layout_1/MainActivity.java
+++ app/src/main/java/com/example/layout_1/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hello world ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello world i am on github ", Toast.LENGTH_SHORT).show();
+
     }
 }
